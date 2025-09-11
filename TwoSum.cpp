219e01f5cf2ed6f6414fc1502abab1de0000fe88
ECSS11 <53446819+Ecss11@users.noreply.github.com>
2025-09-11 08:27:40 -0500

    feat: two sum

---
+++ TwoSum.cpp
@@ -0,0 +1,25 @@
+//
+// Created by ECSS11 on 9/2/2025.
+//
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                }
+            }
+        }
+
+        return result;
+    }
+};
